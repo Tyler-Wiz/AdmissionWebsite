8421bbebd7dd9776ced180d6cce3936421b99d95
tyler <tylertooxclusive@gmail.com>
2021-08-16 10:58:56 +0100

    navbar

---
+++ script.js
@@ -0,0 +1,14 @@
+// const navbar = document.querySelector('#navbar').querySelectorAll('a')
+
+// console.log(navbar)
+
+
+// navbar.forEach(link => {
+//     link.addEventListener('click', function(){
+//         navbar.forEach(nav => nav.classList.remove('active'))
+
+//         this.classList.add('active')
+//     })
+
+
+// })
